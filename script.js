83b6d8ff5e4d10d1ffc1833ae90b019e5f99286d
oscarcastro584 <oscar.castro.ttc.140@unilibre.edu.co>
2024-11-19 22:18:37 -0500

    Prueba 1

---
+++ script.js
@@ -0,0 +1,13 @@
+const form = document.getElementById('contactForm');
+
+form.addEventListener('submit', function(event) {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+
+    if (name === '' || email === '') {
+        alert('Por favor, completa todos los campos.');
+        event.preventDefault();
+    } else {
+        alert('¡Formulario enviado correctamente!');
+    }
+});
